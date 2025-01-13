8ec20dc26063afa18b9bdc629e78798d5ea0b428
陳炫樺 <chenxuanhua456@gmail.com>
2025-01-13 21:11:37 +0800

    映出錯誤訊息

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -12,13 +12,14 @@
     @if($errors->any())
     <div class="alert alert-danger alert-dismissible" role="alert" id="liveAlert">
         <strong>錯誤！</strong> 請修正以下問題：
-        <ul>
-            <li>錯誤 1</li>
-        </ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
     @endif
-    
+    @include('admin.layouts.partials.validation')
+
     <form action="{{ route('admin.posts.store') }}" method="POST" role="form">
         @method('POST')
         @csrf
@@ -42,4 +43,3 @@
     </form>
 </div>
 @endsection
-
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -12,12 +12,13 @@
     @if($errors->any())
     <div class="alert alert-danger alert-dismissible" role="alert" id="liveAlert">
         <strong>錯誤！</strong> 請修正以下問題：
-        <ul>
-            <li>錯誤 1</li>
-        </ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
     @endif
+    @include('admin.layouts.partials.validation')
 
     <form action="{{ route('admin.posts.update', $post->id) }}" method="POST" role="form">
         @method('PATCH')
